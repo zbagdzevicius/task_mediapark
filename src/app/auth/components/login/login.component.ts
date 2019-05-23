08026459af850ee0d99cee0ee0f653eca806a9b6
zbagdzevicius <zbagdzevicius@gmail.com>
2019-05-24 01:00:30 +0300

    login validation + regular expression

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../../services/login/login.service';
+import { Login } from '../../../core/models/login.model';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userLoginForm: FormGroup;
+  login: Login = new Login();
+
+  constructor(private formBuilder: FormBuilder, private loginService: LoginService) {
+    const emailRegEx = /(?=^.{6,25}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/;
+    this.userLoginForm = this.formBuilder.group({
+      email: [this.login.email, Validators.email],
+      password: [this.login.password, Validators.required, Validators.pattern(emailRegEx)]
+    }
+    );
+  }
 
   ngOnInit() {
   }
